4361fec4aad26feb1af7c91c713a9a05172995b6
Mario Portilho <junior.portilho2005@gmail.com>
2025-03-10 16:33:36 +0000

    added remaining models structs

---
+++ repository/models/marketplace/transaction.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Transaction struct {
+	Id int
+	Final_Price int
+	Transaction_Time time.Time
+	Transaction_Type string
+	Seller_Id int
+	Buyer_Id int
+	Listning_Id int
+	Item_Id int
+}
