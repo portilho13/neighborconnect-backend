8a237a5d29c427ab34ea53bb42d9e23c9709b33c
pdqwe2000 <pdqwe2000@gmail.com>
2025-03-20 20:32:58 +0000

    Compare Password functional

--- controllers/client.go
+++ controllers/client.go
@@ -103,7 +103,7 @@ func LoginClient(w http.ResponseWriter, r *http.Request, dbPool *pgxpool.Pool) {
 	}
 
 	// Verify password
-	_, err = utils.ComparePasswordAndHash(creds.Password, dbPool)
+	_, err = utils.ComparePasswordAndHash(creds.Password, user.Password)
 	if err != nil {
 		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
 		return
