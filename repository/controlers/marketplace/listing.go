dcc85729e788d9ba7fc4612697a40089a6715206
Mario Portilho <junior.portilho2005@gmail.com>
2025-04-18 17:08:48 +0100

    changed logic in listing db

--- repository/controlers/marketplace/listing.go
+++ repository/controlers/marketplace/listing.go
@@ -99,3 +99,57 @@ func GetAllListings(dbPool *pgxpool.Pool) ([]models.Listing, error) {
 
 	return listings, nil
 }
+
+func GetAllActiveListings(dbPool *pgxpool.Pool) ([]models.Listing, error) {
+	query := `SELECT 
+	id, name, description, buy_now_price, start_price, created_at, expiration_time, status, seller_id
+	FROM marketplace.listing WHERE status = 'active'`
+
+	rows, err := dbPool.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var listings []models.Listing
+
+	for rows.Next() {
+		var listing models.Listing
+
+		err := rows.Scan(
+			&listing.Id,
+			&listing.Name,
+			&listing.Description,
+			&listing.Buy_Now_Price,
+			&listing.Start_Price,
+			&listing.Created_At,
+			&listing.Expiration_Time,
+			&listing.Status,
+			&listing.Seller_Id,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		listings = append(listings, listing)
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return listings, nil
+}
+
+func UpdateListingStatus(status string, id int, dbPool *pgxpool.Pool) error {
+	query := `UPDATE marketplace.listing SET status = $1 WHERE listing_id = $2`
+
+	_, err := dbPool.Exec(context.Background(), query, status, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
