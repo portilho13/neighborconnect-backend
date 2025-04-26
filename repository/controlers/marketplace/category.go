0cb78cade7643a2c38a3e0a2a565078414e5e40e
Mario Portilho <junior.portilho2005@gmail.com>
2025-04-26 23:09:36 +0100

    fetch category from db

---
+++ repository/controlers/marketplace/category.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	models "github.com/portilho13/neighborconnect-backend/repository/models/marketplace"
+)
+
+func CreateCategory(category models.Category, dbPool *pgxpool.Pool) error {
+	query := `INSERT INTO marketplace.category (name) 
+	VALUES ($1)`
+
+	_, err := dbPool.Exec(context.Background(), query,
+		category.Name,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetAllCategories(dbPool *pgxpool.Pool) ([]models.Category, error) {
+	var categories []models.Category
+
+	query := `SELECT id, name FROM marketplace.category`
+
+	rows, err := dbPool.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var category models.Category
+
+		err := rows.Scan(
+			&category.Id,
+			&category.Name,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
