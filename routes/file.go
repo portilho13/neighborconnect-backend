1f290e7bc46da3215a01f7df95b89d921d36b5ea
portilho13 <junior.portilho2005@gmail.com>
2025-05-02 01:35:55 +0100

    added events and categories to static file serving thorugh api

--- routes/file.go
+++ routes/file.go
@@ -3,5 +3,7 @@ package routes
 import "net/http"
 
 func ServerFilesApi(mux *http.ServeMux) {
-	mux.Handle("/api/v1/uploads/", http.StripPrefix("/api/v1/uploads/", http.FileServer(http.Dir("./uploads"))))
+	mux.Handle("/api/v1/uploads/listing/", http.StripPrefix("/api/v1/uploads/listing/", http.FileServer(http.Dir("./uploads/listing"))))
+	mux.Handle("/api/v1/uploads/event/", http.StripPrefix("/api/v1/uploads/event/", http.FileServer(http.Dir("./uploads/event"))))
+	mux.Handle("/api/v1/uploads/category/", http.StripPrefix("/api/v1/uploads/category/", http.FileServer(http.Dir("./uploads/category"))))
 }
