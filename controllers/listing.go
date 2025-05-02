a78c7807c63aea430de66d57d7674e25551a596d
portilho13 <junior.portilho2005@gmail.com>
2025-05-02 01:43:54 +0100

    removed unecessary code

--- controllers/listing.go
+++ controllers/listing.go
@@ -122,46 +122,6 @@ func CreateListing(w http.ResponseWriter, r *http.Request, dbPool *pgxpool.Pool)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Listing Created!"})
 }
 
-// func CreateListing(w http.ResponseWriter, r *http.Request, dbPool *pgxpool.Pool) {
-// 	var listing controllers_models.ListingCreation
-// 	err := json.NewDecoder(r.Body).Decode(&listing)
-
-// 	if err != nil {
-// 		http.Error(w, "Invalid JSON Data", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	var sellerID *int
-
-// 	if listing.Seller_Id == 0 { // For testing purposes only
-// 		sellerID = nil
-// 	} else {
-// 		sellerID = &listing.Seller_Id
-// 	}
-
-// 	listingDB := models.Listing{
-// 		Name:            listing.Name,
-// 		Description:     listing.Description,
-// 		Buy_Now_Price:   listing.Buy_Now_Price,
-// 		Start_Price:     listing.Start_Price,
-// 		Created_At:      time.Now(),
-// 		Expiration_Date: listing.Expiration_Date,
-// 		Status:          "active", // When a listing is created status will be active by default
-// 		Seller_Id:       sellerID,
-// 	}
-
-// 	err = repositoryControllers.CreateListing(listingDB, dbPool)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		http.Error(w, "Error creating item", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(map[string]string{"message": "Listing Created !"})
-// }
-
 func GetListingById(w http.ResponseWriter, r *http.Request, dbPool *pgxpool.Pool) {
 	query := r.URL.Query()
 	idStr := query.Get("id")
